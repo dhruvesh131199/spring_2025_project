44bc7c1a32f570e555cef6889cca4090ff2e55b3
dhruvesh131199 <dhruveshccc2200@gmail.com>
2025-05-10 10:42:26 -0600

    okay

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import yfinance as yf 
 
-data = yf.download("AAPL", start = "2025-01-01", end = "2025-01-01")
+data = yf.download("AAPL", start = "2025-01-01", end = "2025-01-01", auto_adjust = False)
 
 print(data.head())
