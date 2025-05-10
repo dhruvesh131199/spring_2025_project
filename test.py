5d0c2b927bd6f36dce47553e62f78ef952eba103
dhruvesh131199 <dhruveshccc2200@gmail.com>
2025-05-10 11:13:44 -0600

    Okay

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ all_data = []
 
 for symbol in symbols:
 	print("fetching the data for ",symbol)
-	stock_data = yf.download(symbol, start = "2025-01-01", end = "2025-05-01")
+	stock_data = yf.download(symbol, start = "2025-01-01", end = "2025-05-01", auto_adjust = False)
 	stock_data.reset_index(inplace=True)
 	stock_data["stock_symbol"] = symbol
 	stock_data.columns = ["date", "close", "high", "low", "open", "volume", "symbol"]
